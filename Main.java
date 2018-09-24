f2233a26acc1043d452bcb96079badf82de478f7
Nour <nsaffour@gmail.com>
2018-09-24 21:38:09 +0200

    sorted

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
-	import java.util.*;
-	import java.io.*;
-	import java.util.regex.Pattern;
+import java.util.*;
+import java.io.*;
+import java.util.regex.Pattern;
 
 public class Main {
 	static final int INPUT_SET_SIZE = 10;
@@ -26,46 +26,49 @@ public class Main {
 		return nextCharIsDigit(in) || nextCharIsLetter(in);
 	} 
 
-	static boolean startsWithLetter(String s) {
-		return Pattern.compile("^[a-zA-Z]").matcher(s).find();
+	static int compareTo(Identifier id, Identifier idOther) {
+		return id.toString().compareTo(idOther.toString());
 	}
 
-	static boolean containsNonAlphanumeric(String s) {
-		for (int i = 0; i < s.length(); i++) {
-			if (!Character.isLetterOrDigit(s.charAt(i))) {
-				return true;
-			}
-		}
-		return false;
+	static int compareToLowerCase(Identifier id, Identifier idOther) {
+		return id.toString().toLowerCase().compareTo(idOther.toString().toLowerCase());
 	}
 
-	static String parseString(String s) {
-		s = s.substring(1, s.length() - 1);
-		s = s.replaceAll("(^\\s+|\\s+$)", "");
-		return s;
-	}
-
-
-	static void readFile(String s, Set set) throws Exception {
+	static void readFile(String s, Set set, Map<String, Integer> occurence) throws Exception {
 		Scanner in = new Scanner(new File(s));
 		in.useDelimiter("");
 		while(in.hasNext()) {
 			if(nextCharIsLetter(in)) {
-				addIdentifer(in, set);
+				addIdentifer(in, set, occurence);
 			}
 			nextChar(in);
 		}
-		System.out.printf("the set is  %s \n", set.toString());
+		//System.out.printf("the set is  %s \n", set.toString());
 		in.close();
 
 	}
 	
-	static void addIdentifer(Scanner in, Set set) {
+	static void addIdentifer(Scanner in, Set set, Map<String, Integer> occurence) {
 		Identifier newIdentifer = new Identifier(nextChar(in));
 		while(nextCharIsAlpha(in)) {
 			newIdentifer.append(nextChar(in));
 		}
+		if(occurence.get(newIdentifer.toString()) == null) {
+			occurence.put(newIdentifer.toString(), 0);
+		}
+		int i = occurence.get(newIdentifer.toString());
+		occurence.put(newIdentifer.toString(), i + 1);
 		set.append(newIdentifer);
+	}
+
+	static void sortAndPrint(Map<String, Integer> occurence) {
+		SortedSet<String> keys = new TreeSet<>(occurence.keySet());
+		for (String key : keys) { 
+			int value = occurence.get(key);
+			if((value % 2) != 0) {
+				System.out.printf("%s \n", key);
+			}
+		}
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -75,7 +78,9 @@ public class Main {
 			System.out.println("Please provide a file");
 			return;
 		}
-		readFile(args[0], set1);
-		readFile(args[1], set2);
+		Map<String, Integer> occurence = new HashMap<String, Integer>();
+		readFile(args[0], set1, occurence);
+		readFile(args[1], set2, occurence);
+		sortAndPrint(occurence);
 	}
 }
